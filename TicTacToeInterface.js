8878406d530770a0ec2d4f11ee38b206cec65aae
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-03-09 20:55:59 +0000

    Add start game functionality

--- TicTacToeInterface.js
+++ TicTacToeInterface.js
@@ -2,6 +2,16 @@ $( document ).ready(function() {
 
   $("#player-submit").click(function(){
     event.preventDefault();
-    console.log("It works!!!")
+    names_array =  $('#name_form').serializeArray()
+    player1_name = names_array[0]['value']
+    player2_name = names_array[1]['value']
+
+    game = new Game( player1_name, player2_name)
+
+    $('#name_form').hide()
+    message = player1_name + " vs " + player2_name
+    $('#welcome_message').text(message);
+    $('#welcome_message').show();
+
     });
 });
